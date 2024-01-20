f0c2ee9019dde6b04c2b822ec8b69131027bbfcc
jpaulocts <jpaulocts@gmail.com>
2024-01-20 18:19:57 -0300

    adjusting branch

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,8 @@
-import { setLocalStorage, getLocalStorage, getParams} from "./utils.mjs";
+import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParams('product');
+const productId = getParams("product");
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-
-

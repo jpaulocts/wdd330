03a64b17877dc6286ada0ffc5dfd58a9774ad284
jpaulocts <jpaulocts@gmail.com>
2024-01-20 17:58:49 -0300

    new branch

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,11 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+export function getParams(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const parameter = urlParams.get(param);
+  return parameter;
+
+}

a0ca516193abd27c7ee2df559382b2b6fcf011ff
jpaulocts <jpaulocts@gmail.com>
2024-02-01 23:00:50 -0300

    updating

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -14,9 +14,26 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <div class="product-detail__add">
+      <button class="removeItem" data-id="${item.Id}">Remove item</button>
+    </div>
 </li>`;
 
   return newItem;
+
+
+}
+
+// function to show the total in the cart.
+
+ function sumPrice(list) {
+  const prices = list.reduce((acumulator, element)=> parseFloat(acumulator) + parseFloat(element.FinalPrice),0);
+  console.log(prices);
+  const htmlElement = document.querySelector(".product-list");
+  const sum = document.createElement("span");
+  sum.innerHTML = `Total: $${prices}`;
+  htmlElement.appendChild(sum);
+
 }
 
 export default class ShoppingCart {
@@ -27,6 +44,27 @@ export default class ShoppingCart {
   renderCartContents() {
     const cartItems = getLocalStorage(this.key);
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
+    document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");  
+    this.setupRemoveItemListeners(cartItems); 
+    sumPrice(cartItems);
+
+   
+
   }
+
+  setupRemoveItemListeners(cartItems) {
+    const removeButtons = document.querySelectorAll(".removeItem");
+    
+    removeButtons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        const itemId = event.currentTarget.dataset.id;
+        // Remove the item from the array
+        const updatedCartItems = cartItems.filter(item => item.Id !== itemId);
+        // Update the local storage and re-render the cart
+        localStorage.setItem(this.key, JSON.stringify(updatedCartItems));
+        this.renderCartContents();
+      });
+    });
+  }
+
 }
